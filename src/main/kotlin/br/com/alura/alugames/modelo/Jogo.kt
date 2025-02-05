ab28b4202a05fdd3675d7243022e3f8734a56fd7
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-02-05 08:14:44 -0300

    Refatoração do código

--- src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
@@ -1,11 +1,13 @@
-data class Jogo ( val titulo:String,
-             val capa:String) {
+package br.com.alura.alugames.modelo
+
+data class Jogo (val titulo:String,
+                 val capa:String) {
 
     var descricao:String? = null
 
     override fun toString(): String {
 
-        return "Meu Jogo: \n" +
+        return "Meu br.com.alura.alugames.modelo.Jogo: \n" +
                 "Título: $titulo \n" +
                 "Capa: $capa \n" +
                 "Descrição: $descricao"
---
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -0,0 +1,50 @@
+package br.com.alura.alugames.principal
+
+import br.com.alura.alugames.modelo.*
+import br.com.alura.alugames.servicos.ConsumoApi
+import java.util.Scanner
+
+fun main() {
+
+    val leitura = Scanner(System.`in`)
+    println("Digite o ID do jogo que deseja buscar:")
+    val busca = leitura.nextLine()
+
+    var meuJogo: Jogo? = null
+
+    val resultado = runCatching {
+
+        val buscaApi = ConsumoApi()
+        val meuInfoJogo = buscaApi.buscaJogo(busca)
+
+        meuJogo = Jogo(
+            meuInfoJogo.info.title,
+            meuInfoJogo.info.thumb)
+    }
+
+    resultado.onFailure {
+        println("Erro ao buscar o jogo, tente outro ID.")
+    }
+
+    resultado.onSuccess {
+
+        println("Deseja inserir uma descrição personalizada? (s/n)")
+        val resposta = leitura.nextLine()
+
+        if (resposta.equals("s", true)) {
+            println("Digite a descrição personalizada:")
+            val descricaoPersonalizada = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizada
+            println("Descrição inserida: $descricaoPersonalizada")
+        } else {
+            meuJogo?.descricao = meuJogo?.titulo
+
+        }
+
+        println(meuJogo)
+    }
+
+    resultado.onSuccess {
+        println("Busca finalizada com sucesso")
+    }
+}
