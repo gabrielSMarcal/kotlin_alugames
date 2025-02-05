9a38e8e3633f35d0f9cf848cedebb085f5dee0f9
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-02-05 07:34:03 -0300

    Tratando Exceptions com runCatching

--- src/main/kotlin/Jogo.kt
+++ src/main/kotlin/Jogo.kt
@@ -1,7 +1,7 @@
 data class Jogo ( val titulo:String,
              val capa:String) {
 
-    val descricao = ""
+    var descricao:String? = null
 
     override fun toString(): String {
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,80 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
+import java.util.Scanner
 
 
 fun main() {
 
+    val leitura = Scanner(System.`in`)
+    println("Digite o ID do jogo que deseja buscar:")
+    val busca = leitura.nextLine()
+
+    val endereco = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(endereco))
         .build()
-    val response = client
-        .send(request, BodyHandlers.ofString())
 
-    val json = response.body()
-    println(json)
+//    try {
+//        val response = client
+//        .send(request, BodyHandlers.ofString())
+//
+//    val json = response.body()
+//    println(json)
+//
+//    val gson = Gson()
+//    val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+//        val meuJogo = Jogo(
+//            meuInfoJogo.info.title,
+//            meuInfoJogo.info.thumb)
+//
+//        println(meuJogo)
+//    } catch (ex: Exception) {
+//        println("Erro ao buscar o jogo, tente outro ID.")
+//    }
 
-    val gson = Gson()
-    val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+    var meuJogo:Jogo? = null
 
-    val meuJogo = Jogo(meuInfoJogo.info.title, meuInfoJogo.info.thumb)
-    println(meuJogo)
+    val resultado = runCatching {
+
+        val response = client
+            .send(request, BodyHandlers.ofString())
+
+        val json = response.body()
+//        println(json)
+
+        val gson = Gson()
+        val meuInfoJogo = gson.fromJson(json, InfoJogo::class.java)
+
+        meuJogo = Jogo(
+            meuInfoJogo.info.title,
+            meuInfoJogo.info.thumb)
+    }
+
+    resultado.onFailure {
+        println("Erro ao buscar o jogo, tente outro ID.")
+    }
+
+    resultado.onSuccess {
+        println("Deseja inserir uma descrição personalizada? (s/n)")
+        val resposta = leitura.nextLine()
+
+        if (resposta.equals("s", true)) {
+            println("Digite a descrição personalizada:")
+            val descricaoPersonalizada = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizada
+            println("Descrição inserida: $descricaoPersonalizada")
+        } else {
+            meuJogo?.descricao = meuJogo?.titulo
+
+        }
+
+        println(meuJogo)
+    }
+
+    resultado.onSuccess {
+        println("Busca finalizada com sucesso")
+    }
 }
