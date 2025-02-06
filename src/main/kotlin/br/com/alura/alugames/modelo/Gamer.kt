689a671ca650a59733a4a208211e6ca4e2ad8e11
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-02-06 06:40:49 -0300

    Criando gerador de ID interno

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,5 +1,7 @@
 package br.com.alura.alugames.modelo
 
+import kotlin.random.Random
+
 data class Gamer(var nome: String, var email: String) {
 
     var dataNascimento: String? = null
@@ -11,10 +13,19 @@ data class Gamer(var nome: String, var email: String) {
 
         this.dataNascimento = dataNascimento
         this.usuario = usuario
+        criarIdInterno()
     }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento," +
                 " usuario=$usuario, idInterno=$idInterno)"
+    }
+
+    fun criarIdInterno() {
+
+        val numero = Random.nextInt(10000)
+        val tag = String.format("%04d", numero)
+
+        idInterno = "$usuario#$tag"
     }
 }
