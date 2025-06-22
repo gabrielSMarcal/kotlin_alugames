3369d2c39abefc024d1a165c2d100d120a4b79a9
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-06-22 09:52:48 -0300

    Adiciona cálculo do valor do aluguel com base no período e ajusta assinatura do método `alugaJogo`.

--- src/main/kotlin/br/com/alura/alugames/modelo/Aluguel.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Aluguel.kt
@@ -1,9 +1,19 @@
 package br.com.alura.alugames.modelo
 
-data class Aluguel(val gamer: Gamer,
-                   val jogo: Jogo) {
+import java.time.LocalDate
+import java.time.Period
+
+data class Aluguel(
+    val gamer: Gamer,
+    val jogo: Jogo,
+    val dataInicial: LocalDate,
+    val dataFinal: LocalDate) {
+
+    val valorDoAluguel = jogo.preco * Period.between(dataInicial, dataFinal).days
+
+
 
     override fun toString(): String {
-        return "Aluguel do ${jogo.titulo} por ${gamer.nome}"
+        return "Aluguel do ${jogo.titulo} por ${gamer.nome}, pelo valor de R$$valorDoAluguel"
     }
 }
