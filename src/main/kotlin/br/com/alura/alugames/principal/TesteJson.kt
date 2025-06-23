9d8dc5fd0eba4706e63bde120b7299f5e9ede1bb
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-06-23 17:57:44 -0300

    Adiciona anotação @Expose na classe Jogo e serializa recomendações em arquivos JSON

--- src/main/kotlin/br/com/alura/alugames/principal/TesteJson.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteJson.kt
@@ -3,6 +3,8 @@ package br.com.alura.alugames.principal
 import br.com.alura.alugames.modelo.Periodo
 import br.com.alura.alugames.modelo.PlanoAssinatura
 import br.com.alura.alugames.servicos.ConsumoApi
+import com.google.gson.GsonBuilder
+import java.io.File
 import java.time.LocalDate
 import java.time.Period
 
@@ -18,9 +20,13 @@ fun main() {
 
     val gamerCaroline = listaGamers.get(3)
     val jogoResidentVillage = listaJogoJson.get(10)
-
     val jogoSpider = listaJogoJson.get(13)
     val jogoTLOU = listaJogoJson.get(2)
+    val jogoDandara = listaJogoJson.get(5)
+    val jogoAssassins = listaJogoJson.get(4)
+    val jogoCyber = listaJogoJson.get(6)
+    val jogoGod = listaJogoJson.get(7)
+    val jogoSkyrim = listaJogoJson.get(18)
 
 
 //    println(gamerCaroline)
@@ -44,30 +50,39 @@ fun main() {
     val gamerCamila = listaGamers.get(5)
     gamerCamila.plano = PlanoAssinatura("Prata", 9.90, 3, 0.15)
 
-    gamerCamila.alugaJogo(jogoResidentVillage, periodo1)
-    gamerCamila.alugaJogo(jogoSpider, periodo2)
-    gamerCamila.alugaJogo(jogoTLOU, periodo3)
-    gamerCamila.alugaJogo(jogoTLOU, periodo3)
-    println(gamerCamila.jogosAlugados)
-
-    gamerCamila.recomendar(7)
-    gamerCamila.recomendar(10)
-    gamerCamila.recomendar(8)
-    println(gamerCamila)
-
-    gamerCamila.alugaJogo(jogoResidentVillage, periodo1)
-    println(gamerCamila.jogosAlugados)
+//    gamerCamila.alugaJogo(jogoResidentVillage, periodo1)
+//    gamerCamila.alugaJogo(jogoSpider, periodo2)
+//    gamerCamila.alugaJogo(jogoTLOU, periodo3)
+//    gamerCamila.alugaJogo(jogoTLOU, periodo3)
+//    println(gamerCamila.jogosAlugados)
+//
+//    gamerCamila.recomendar(7)
+//    gamerCamila.recomendar(10)
+//    gamerCamila.recomendar(8)
+//    println(gamerCamila)
+//
+//    gamerCamila.alugaJogo(jogoResidentVillage, periodo1)
+//    println(gamerCamila.jogosAlugados)
+//
+//    gamerCamila.recomendarJogo(jogoResidentVillage, 7)
+//    gamerCamila.recomendarJogo(jogoTLOU, 10)
+//
+//    gamerCaroline.recomendarJogo(jogoResidentVillage, 8)
+//    gamerCaroline.recomendarJogo(jogoTLOU, 8)
 
     gamerCamila.recomendarJogo(jogoResidentVillage, 7)
     gamerCamila.recomendarJogo(jogoTLOU, 10)
+    gamerCamila.recomendarJogo(jogoAssassins, 8)
+    gamerCamila.recomendarJogo(jogoCyber, 7)
+    gamerCamila.recomendarJogo(jogoGod, 10)
+    gamerCamila.recomendarJogo(jogoDandara, 8)
+    gamerCamila.recomendarJogo(jogoSkyrim, 8)
+    gamerCamila.recomendarJogo(jogoSpider, 6)
 
-    gamerCaroline.recomendarJogo(jogoResidentVillage, 8)
-    gamerCaroline.recomendarJogo(jogoTLOU, 8)
+    val gson = GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+    val serializacao = gson.toJson(gamerCamila.jogosRecomendados)
 
-    println("Recomendações Camila")
-    println(gamerCamila.jogosRecomendados)
-    println("Recomendações Caroline")
-    println(gamerCaroline.jogosRecomendados)
-
-
+    val arquivo = File("jogosRecomendados-${gamerCamila.nome}.json")
+    arquivo.writeText(serializacao)
+    println(arquivo.absolutePath)
 }
