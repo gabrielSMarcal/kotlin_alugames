cdecdf011f0d4cd5b0f50e8c15b1dfb50540e63f
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-06-24 19:36:35 -0300

    Adiciona dependência do MySQL e método para obter conexão com o banco de dados

--- src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
@@ -1,4 +1,24 @@
 package br.com.alura.alugames.dados
 
-class Banco {
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+object Banco {
+
+    fun obterConexao(): Connection? {
+
+        val dbPassword = System.getenv("MYSQL_PASS")
+
+        return try {
+            DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/alugames",
+            "root",
+            dbPassword)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            null
+        }
+
+    }
 }
