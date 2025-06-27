33defe9f78ec3ff516d44f2e32d9578983e418b9
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-06-27 17:41:47 -0300

    Extrai interface genérica `DAO` e refatora `JogosDAO` e `GamersDAO` para herdar métodos padrão.

--- src/main/kotlin/br/com/alura/alugames/dados/GamersDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/GamersDAO.kt
@@ -3,16 +3,16 @@ package br.com.alura.alugames.dados
 import br.com.alura.alugames.modelo.Gamer
 import javax.persistence.EntityManager
 
-class GamersDAO(private val manager: EntityManager) {
+class GamersDAO(private val manager: EntityManager): DAO<Gamer>() {
 
-    fun getGamers(): List<Gamer> {
+    override fun getLista(): List<Gamer> {
 
         val query = manager.createQuery("FROM GamerEntity", GamerEntity::class.java)
         return query.resultList.map { entity -> Gamer(entity.nome, entity.email,
             entity.dataNascimento, entity.usuario, entity.id) }
     }
 
-    fun adicionarGamer(gamer: Gamer) {
+    override fun adicionar(gamer: Gamer) {
 
         val entity = GamerEntity(gamer.id, gamer.nome, gamer.email,
             gamer.dataNascimento, gamer.usuario)
