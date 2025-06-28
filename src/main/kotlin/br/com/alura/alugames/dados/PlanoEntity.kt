5e49ae84b7221b8223045670b37bb51d5c793a4f
yhamerith <gabriel.santos.marcar1@gmail.com>
2025-06-28 11:19:01 -0300

    Adiciona anotações JPA em `PlanoEntity` e cria mapeamento para herança com discriminador `tipoPlano`

--- src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/PlanoEntity.kt
@@ -1,9 +1,25 @@
 package br.com.alura.alugames.dados
 
+import javax.persistence.DiscriminatorColumn
+import javax.persistence.DiscriminatorType
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
+import javax.persistence.Inheritance
+import javax.persistence.InheritanceType
+import javax.persistence.Table
+
+@Entity
+@Table (name = "planos")
+@Inheritance (strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipoPlano", discriminatorType = DiscriminatorType.STRING)
 sealed class PlanoEntity(val tipo :String)
 
+@Entity
+@DiscriminatorValue("Avuls")
 class PlanoAvulsoEntity(tipo: String): PlanoEntity(tipo)
 
+@Entity
+@DiscriminatorValue("Assinatura")
 class PlanoAssunaturaEntity(
     tipo: String,
     val mendalidade: Double,
